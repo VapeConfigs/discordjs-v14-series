f74c0a3f2587df960dedee7e814977c436b192e1
VapeConfigs <157316646+VapeConfigs@users.noreply.github.com>
2024-05-19 09:33:34 -0400

    Update index.js

--- 06 - Custom Status/src/index.js
+++ 06 - Custom Status/src/index.js
@@ -9,29 +9,14 @@ const client = new Client({
     IntentsBitField.Flags.MessageContent,
   ],
 });
-
-let status = [
-  {
-    name: 'Under Ctrl',
-    type: ActivityType.Streaming,
-    url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
-  },
-  {
-    name: 'Custom Status 1',
-  },
-  {
-    name: 'Custom Status 2',
-    type: ActivityType.Watching,
-  },
-  {
-    name: 'Custom Status 3',
-    type: ActivityType.Listening,
-  },
-];
-
+   
 client.on('ready', (c) => {
   console.log(`✅ ${c.user.tag} is online.`);
 
+client.user.setActivity({
+name: "Bypassing
+})
+  
   setInterval(() => {
     let random = Math.floor(Math.random() * status.length);
     client.user.setActivity(status[random]);
